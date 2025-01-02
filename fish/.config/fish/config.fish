597dcb23c0ec44424f0bb145250a7628a0a4ba56
Tobias Dünser <tobias@kerfuffle.club>
2025-01-02 01:02:52 +0100

    fix fzf script

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -7,10 +7,15 @@ fzf --fish | source
 
 function good_cd;
 	set chosen_dir (fzf --walker dir,follow,hidden  --walker-skip .git,node_modules,target,env)
-	if set -q chosen_dir;
+	if [ $chosen_dir ];
 		cd $chosen_dir
-		commandline -f repaint;
+		commandline -f repaint
 	end
+
 end
+
+function debug;
+	echo $(pwd);
+end;
 
 bind \ce good_cd
