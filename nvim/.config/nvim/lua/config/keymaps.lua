aaa581d788cf7b198fc3981d7adbce9ddbb2e845
Tobias Dünser <tobias@kerfuffle.club>
2025-01-25 20:07:42 +0100

    add java to nvim

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,4 @@
 local map = LazyVim.safe_keymap_set
-map({'i'}, '<C-s>', '<C-o>:w<ENTER>')
-map({'n'}, '<C-s>', ':w<ENTER>')
+map({ "i" }, "<C-s>", "<C-o>:w<ENTER>")
+map({ "n" }, "<C-s>", ":w<ENTER>")
+map({ "n" }, "o", "<cmd>Neotree focus<ENTER>")
