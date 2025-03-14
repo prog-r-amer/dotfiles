a142c7b909276e76a99924d0cb729d2d54dc3b1c
Tobias Dünser <tobias@kerfuffle.club>
2025-03-14 15:08:29 +0100

    add emsdk to fish config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,12 +1,14 @@
 if status is-interactive
     set -Ux MANPAGER 'nvim +Man!'
-    fish_add_path "/home/pingutz/.local/bin"
+    fish_add_path /home/pingutz/bin
+    fish_add_path /home/pingutz/.local/bin
 end
 
 starship init fish | source
 fzf --fish | source
 set -gx EDITOR nvim
 alias jb="just build"
+alias se="sudoedit"
 
 function good_cd
     set chosen_dir (fzf --walker dir,follow,hidden  --walker-skip .git,node_modules,target,env)
@@ -18,3 +20,13 @@ function good_cd
 end
 
 bind \ce good_cd
+
+# >>> coursier install directory >>>
+set -gx PATH "$PATH:/home/pingutz/.local/share/coursier/bin"
+# <<< coursier install directory <<<
+pyenv init - | source
+set -gx EMSDK_QUIET 1
+
+if test -e "$HOME/.config/fish/emsdk.fish"
+    source $HOME/.config/fish/emsdk.fish
+end
