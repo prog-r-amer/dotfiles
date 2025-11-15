4712a19e15fd94d44f29e054627b27ad3ba3a29e
Tobias Dünser <tobias@kerfuffle.club>
2025-11-15 23:06:01 +0100

    configure mini.files

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -3,6 +3,19 @@ map({ "i" }, "<C-s>", "<C-o>:w<ENTER>")
 map({ "n" }, "<C-s>", ":w<ENTER>")
 
 vim.keymap.set("n", "<leader>e", function()
-	local miniFiles = require("mini.files")
-	miniFiles.open()
+	local MiniFiles = require("mini.files")
+	if MiniFiles.get_explorer_state() ~= nil then
+		MiniFiles.close()
+	end
+	local file = vim.api.nvim_buf_get_name(0)
+
+	-- If no file is loaded (e.g. an empty buffer), fall back to CWD
+	local dir
+	if file == "" then
+		dir = vim.uv.cwd()
+	else
+		dir = vim.fn.fnamemodify(file, ":p:h")
+	end
+
+	MiniFiles.open(dir, false)
 end, { desc = "Open File Browser" })
