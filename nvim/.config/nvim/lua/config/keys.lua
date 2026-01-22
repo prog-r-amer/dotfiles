f85f79ba9644e01bfc1442adbf3a81c9194b44fb
Tobias Dünser <tobias.duenser@xitrust.com>
2026-01-22 13:35:15 +0100

    dont close terminal buffers with <leader>bo

--- nvim/.config/nvim/lua/config/keys.lua
+++ nvim/.config/nvim/lua/config/keys.lua
@@ -22,7 +22,7 @@ end, { desc = "Delete buffer" })
 vim.keymap.set("n", "<leader>bo", function()
 	local current = vim.api.nvim_get_current_buf()
 	for _, buf in ipairs(vim.api.nvim_list_bufs()) do
-		if buf ~= current and vim.api.nvim_buf_is_loaded(buf) then
+		if buf ~= current and vim.api.nvim_buf_is_loaded(buf) and vim.bo[buf].buftype ~= "terminal" then
 			vim.api.nvim_buf_delete(buf, { force = true })
 		end
 	end
