888dbba31648b383af8f20a67c27718003be84e7
Pingutz <tobias@kerfuffle.club>
2026-03-02 19:07:53 +0100

    cleanup

--- nvim/.config/nvim/lua/plugins/bufferline.lua
+++ nvim/.config/nvim/lua/plugins/bufferline.lua
@@ -1,14 +1,3 @@
-local function focus_non_terminal_window()
-	for _, win in ipairs(vim.api.nvim_list_wins()) do
-		local buf = vim.api.nvim_win_get_buf(win)
-
-		if vim.bo[buf].buftype ~= "terminal" then
-			vim.api.nvim_set_current_win(win)
-			return
-		end
-	end
-end
-
 return {
 	{
 		"akinsho/bufferline.nvim",
@@ -18,10 +7,6 @@ return {
 				always_show_bufferline = true,
 				show_buffer_icons = false,
 				show_buffer_close_icons = false,
-				left_mouse_command = function(bufnr)
-					focus_non_terminal_window()
-					vim.cmd("buffer " .. bufnr)
-				end,
 			},
 		},
 		config = function(_, opts)
