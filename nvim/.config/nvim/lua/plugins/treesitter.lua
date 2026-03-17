edf00331b9595a3a5a9e9218e12e624d63eb3a53
Tobias Dünser <tobias.duenser@xitrust.com>
2026-03-17 13:31:55 +0100

    fix treesitter textobjects

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,8 +1,24 @@
 return {
 	{
+		"nvim-treesitter/nvim-treesitter-textobjects",
+	},
+	{
 		"nvim-treesitter/nvim-treesitter",
 		build = ":TSUpdate",
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter-textobjects",
+		},
 		config = function()
+			local move = require("nvim-treesitter-textobjects.move")
+
+			vim.keymap.set("n", "<leader>w", function()
+				move.goto_next_start("@argument.outer")
+			end)
+
+			vim.keymap.set("n", "<leader>b", function()
+				move.goto_previous_start("@argument.outer")
+			end)
+
 			vim.api.nvim_create_autocmd("FileType", {
 				callback = function(ev)
 					if vim.bo[ev.buf].buftype ~= "" then
@@ -12,20 +28,5 @@ return {
 				end,
 			})
 		end,
-	},
-	{
-		"nvim-treesitter/nvim-treesitter-textobjects",
-		opts = {
-			textobjects = {
-				move = {
-					enable = true,
-					set_jumps = true,
-					goto_next_start = {
-						["<leader>w"] = "@argument.outer",
-					},
-					goto_previous_start = { ["<leader>b"] = "@argument.outer" },
-				},
-			},
-		},
 	},
 }
